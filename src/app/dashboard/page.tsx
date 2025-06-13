412f13600f04c3041f79576bee8bc743af09f7b2
smb-lemon <smblemon9@gmail.com>
2025-06-13 20:11:50 +0600

    feat: add charts component with dependencies

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import { DashboardCards } from "./dashboard-cards";
+import { Charts } from "./dashboard-charts";
+
 export default function HomePage() {
   return (
     <div>
       <h1 className="text-2xl font-bold">Dashboard Overview</h1>
       {/* Add dashboard cards or charts here */}
+      <DashboardCards />
+      <Charts/>
     </div>
   );
 }
