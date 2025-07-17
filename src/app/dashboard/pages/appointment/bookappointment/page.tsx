a2c7f523863cebb40c711f59dd004b313c51dd2e
smb-lemon <smblemon9@gmail.com>
2025-07-17 15:33:38 +0600

    feat: add BookAppointmentForm with form and textarea component added

---
+++ src/app/dashboard/pages/appointment/bookappointment/page.tsx
@@ -0,0 +1,10 @@
+import { BookAppointmentForm } from './BookAppointmentForm'
+
+export default function BookAppointmentPage() {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold ">Book Appointment</h1>
+        <BookAppointmentForm />
+    </div>
+  )
+}
